e4cb676c34a9b99f0d875dd6abcfa8843978cd52
Galane <welcomegalane@gmail.com>
2025-09-23 21:14:50 +0200

    created new error container file

---
+++ front-end/src/components/ErrorContainer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useError } from "../context/ErrorContext";
+import ErrorToast from "./Error";
+import "./Error.css";
+
+const ErrorContainer: React.FC = () => {
+  const { errors, removeError } = useError();
+  console.log('ErrorContainer errors:', errors);
+
+  if (errors.length === 0) return null;
+
+  return (
+    <div className="error-container">
+      {errors.map((error, index) => (
+        <div 
+          key={error.id} 
+          style={{ 
+            position: 'fixed',
+            top: `${1 + index * 5}rem`,
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 99999 - index
+          }}
+        >
+          <ErrorToast
+            message={error.message}
+            type={error.type}
+            duration={error.duration}
+            onClose={() => removeError(error.id)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ErrorContainer;
